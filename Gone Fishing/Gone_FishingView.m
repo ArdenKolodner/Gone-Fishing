30907ab0106fe176443b80bd5a290c7ef71a587c
Arden Kolodner <akolodner25@amherst.edu>
2023-06-19 23:40:42 -0400

    Initial Commit

---
+++ Gone Fishing/Gone_FishingView.m
@@ -0,0 +1,51 @@
+//
+//  Gone_FishingView.m
+//  Gone Fishing
+//
+//  Created by Arden Kolodner on 6/19/23.
+//
+
+#import "Gone_FishingView.h"
+
+@implementation Gone_FishingView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
